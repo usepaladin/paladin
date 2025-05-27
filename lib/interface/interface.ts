b49d991c9b9e19c23f36c127718af119a36ed4e4
Jared Tucker <jtuc0006@student.monash.edu>
2025-05-27 18:10:50 +1000

    Set up application structure

---
+++ lib/interface/interface.ts
@@ -0,0 +1,27 @@
+import React from "react";
+
+// FCWC: FunctionComponentWithChildren
+export type FCWC<T> = React.FC<React.PropsWithChildren<T>>;
+
+// Propless: Component without props
+export type Propless = {};
+
+export interface ChildNodeProps {
+    children: React.ReactNode;
+}
+
+export interface ClassNameProps {
+    className?: string;
+}
+
+export interface SupabaseClientResponse<T extends Error, V = unknown> {
+    ok: boolean;
+    data?: V;
+    error?: T;
+}
+
+export interface ControllerResponse<T> {
+    status: number;
+    data?: T;
+    error?: string;
+}
